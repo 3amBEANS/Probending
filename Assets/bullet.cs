7ce13a9bfe049c7304aaed7d1e1154c47c4a8c2d
brayzw <braydenwang3@gmail.com>
2023-01-08 21:22:51 -0500

    idk what this is

--- Assets/bullet.cs
+++ Assets/bullet.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,12 +9,22 @@ public class bullet : MonoBehaviour
     Rigidbody rb;
     public GameObject firePoint;
     public int throwForce;
+    int penis = 5;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.AddForce(firePoint.transform.forward * throwForce);
+        Destroy(gameObject, penis);
+        
     }
 
-    
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+
+
 }
